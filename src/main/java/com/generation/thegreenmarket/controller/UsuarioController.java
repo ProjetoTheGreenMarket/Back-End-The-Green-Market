43cdb1d8b1c8604ad315850454a318e2da0677d3
Fewatts <feralveswatts@gmail.com>
2023-05-09 21:38:52 -0300

    Final Task
    Implementação DELETE user

--- src/main/java/com/generation/thegreenmarket/controller/UsuarioController.java
+++ src/main/java/com/generation/thegreenmarket/controller/UsuarioController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.generation.thegreenmarket.model.Usuario;
 import com.generation.thegreenmarket.model.UsuarioLogin;
@@ -64,6 +67,17 @@ public class UsuarioController {
 		return usuarioService.atualizarUsuario(usuario)
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		if(usuario.isEmpty()){
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		usuarioRepository.deleteById(id);
 	}
 
 }
